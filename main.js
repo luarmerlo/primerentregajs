999c7be6de5172830f2d62edfc18a9fc8340a769
luar merlo <luarmerlorevello@hotmail.com>
2023-05-01 16:21:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+/*for(let i = 0; i < 100; i = i + 3) {
+    console.log("hola lain");
+}
+
+alert("hola lain");
+
+ let numero = parseInt(prompt("Ingrese un numero"))
+ 
+ for(let i = 1; i <= 10; i++){
+    let resultado = numero * i;
+
+    alert(`${numero} * ${i} = ${resultado}`);
+ }*/
+
+ function saludar(saludo, nombre) {
+  let mensaje = `${saludo} ${nombre}`;
+  console.log(mensaje);
+ }
+
+ saludar("hola", "camila");
+ saludar("bienvenido", "pepito");
+
+function sumar(num1, num2) {
+    console.log(num1 + num2);
+}
+
+sumar(7, 2);
+
